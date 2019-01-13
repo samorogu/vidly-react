ba9ca3778b75f0c7b4fd09348afff1a99f72df27
™samorogu™ <samorogu@gmail.com>
2019-01-13 15:22:23 -0600

    substituting the methods of props within the sfc parameter

--- src/common/paginantion.jsx
+++ src/common/paginantion.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import _ from "lodash"; //optimization from underscore
 
-const Pagination = props => {
-  const { itemsCount, pageSize, currentPage, onPageChange } = props;
-  console.log(currentPage);
+const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
+  //console.log(currentPage);
   const pagesCount = Math.ceil(itemsCount / pageSize);
 
   if (pagesCount === 1) return null;
