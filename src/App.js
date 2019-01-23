d876105e8771bed593317bee58c25ac6a9bba8b9
™samorogu™ <samorogu@gmail.com>
2019-01-22 23:19:35 -0600

    adding the routes with the expected behavior

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
-import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <main className="container" />
-        <Movies />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="movies" />
+            <Redirect to="not-found" />
+          </Switch>
+        </main>
       </React.Fragment>
     );
   }
