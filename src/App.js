31da508869b55ef2412c27533e29e20644418208
™samorogu™ <samorogu@gmail.com>
2019-01-23 23:52:15 -0600

    adding the navBar component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <NavBar />
         <main className="container">
           <Switch>
             <Route path="/movies" component={Movies} />
