38a6e8339a9b572bc2148689e54e8620326e8a7a
™samorogu™ <samorogu@gmail.com>
2019-01-25 23:44:43 -0600

    adding the link to the movies form rendering the movie id

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Like from "../common/like";
 import Table from "../common/table";
 
 class MoviesTable extends Component {
   //it doesn't need to be within a state because it won't change in lifecycle hooks
   columns = [
-    { path: "title", label: "Title" },
+    {
+      path: "title",
+      label: "Title",
+      content: movie => <Link to={`/movies/${movie._id}`}>{movie.title} </Link>
+    },
     { path: "genre.name", label: "Genre" },
     { path: "numberInStock", label: "Stock" },
     { path: "dailyRentalRate", label: "Rate" },
