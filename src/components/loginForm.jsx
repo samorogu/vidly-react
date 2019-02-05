b60d0d2d447ace486d7681742acb0aa858d3ae4c
™samorogu™ <samorogu@gmail.com>
2019-02-04 23:06:52 -0600

    adding joi package to handle valdation shemas

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "../common/input";
 
 class LoginForm extends Component {
@@ -12,7 +13,17 @@ class LoginForm extends Component {
     errors: {} //we include errors object
   };
 
+  schema = {
+    username: Joi.string().required(),
+    password: Joi.string().required()
+  };
+
   validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false
+    });
+    console.log(result);
+
     const errors = {};
 
     const { account } = this.state;
