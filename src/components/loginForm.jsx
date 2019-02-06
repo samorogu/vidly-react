e33d958ba21bc6c59b60fb41fde0b4d32296db70
™samorogu™ <samorogu@gmail.com>
2019-02-05 23:21:18 -0600

    adding the validation message with the joi details object

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -14,26 +14,38 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required()
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
   };
 
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false
-    });
-    console.log(result);
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    //console.log(result);
+
+    // const errors = {};
+
+    // const { account } = this.state;
+
+    // if (account.username.trim() === "")
+    //   errors.username = "Username is requiered.";
+    // if (account.password.trim() === "")
+    //   errors.password = "Password is requiered.";
+
+    // return Object.keys(errors).length === 0 ? null : errors;
+
+    //now we are going to make the code with joi
+
+    if (!error) return null;
 
     const errors = {};
 
-    const { account } = this.state;
-
-    if (account.username.trim() === "")
-      errors.username = "Username is requiered.";
-    if (account.password.trim() === "")
-      errors.password = "Password is requiered.";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = e => {
