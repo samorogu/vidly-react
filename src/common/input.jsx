29524d8b678d17042a928cbc101d0b191f1822c2
™samorogu™ <samorogu@gmail.com>
2019-02-09 14:52:55 -0600

    extracting the helper rendering methods

--- src/common/input.jsx
+++ src/common/input.jsx
@@ -1,19 +1,10 @@
 import React from "react";
 
-const Input = ({ name, label, value, error, onChange }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value} //props to set its value
-        onChange={onChange} //update the state when the user types
-        // autoFocus
-        // ref={this.username}
-        id={name}
-        name={name}
-        type="text"
-        className="form-control"
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
