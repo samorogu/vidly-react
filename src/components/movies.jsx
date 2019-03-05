e41ed8b4e28f7a4ac830e3f60903ce4c9b45abb5
™samorogu™ <samorogu@gmail.com>
2019-03-04 23:25:31 -0600

    adding the movieService to deal with the api and modifing the movies component to update the getMovies() and deleteMovie() method

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import MoviesTable from "./moviesTable";
 import ListGroup from "../common/listGroup";
 import Pagination from "../common/paginantion";
 import SearchBox from "../common/searchBox";
-import { getMovies } from "../services/fakeMovieService";
+//import { getMovies } from "../services/fakeMovieService";
+import { getMovies, deleteMovie } from "../services/movieService";
 import { getGenres } from "../services/genreService";
 import { paginate } from "../utils/paginate";
 import _ from "lodash";
@@ -23,14 +25,26 @@ class Movies extends Component {
   async componentDidMount() {
     const { data } = await getGenres();
     const genres = [{ _id: "", name: "All Genres" }, ...data];
-    this.setState({ movies: getMovies(), genres });
+
+    const { data: movies } = await getMovies();
+    this.setState({ movies, genres });
   }
 
-  handleDelete = movie => {
-    const movies = this.state.movies.filter(m => m._id !== movie._id);
+  handleDelete = async movie => {
+    const originalMovies = this.state.movies;
+    const movies = originalMovies.filter(m => m._id !== movie._id);
     //in modern javascript when we have this repetition, we can leave only the movie
     //this.setState({ movies: movies });
     this.setState({ movies });
+
+    try {
+      await deleteMovie(movie._id);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        toast.error("This movie has already been deleted.");
+      console.log("entre al error");
+      this.setState({ movies: originalMovies });
+    }
   };
   //this approach is correct but not quite if we dont want to render the table also
   /*
