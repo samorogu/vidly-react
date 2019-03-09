15d27aee15e047f8e78b146b6015565e5dba62ac
™samorogu™ <samorogu@gmail.com>
2019-03-09 08:58:09 -0600

    refactoring the repetitive code for the apiEndpoint+movies

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,24 +2,31 @@ import http from "./httpService";
 import { apiUrl } from "../config.json";
 
 //const apiEndpoint = "http://localhost:3900/api/movies";
+
 const apiEndpoint = apiUrl + "/movies";
+
+function MovieUrl(id) {
+  return `${apiEndpoint}/{id}`; //template numerals in EG6
+  //return "apiEndpoint +'/'+id;";//less elegant solution
+}
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + "/" + movieId);
+  return http.delete(MovieUrl(movieId));
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + "/" + movieId);
+  return http.get(MovieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(apiEndpoint + "/" + movie._id, body);
+    return http.put(MovieUrl(movie._id), body);
   } else {
     return http.post(apiEndpoint, movie);
   }
