ac492d4652c41360c98799887ffe576493a78c25
™samorogu™ <samorogu@gmail.com>
2019-03-11 23:35:09 -0600

    adding the try catch block when submiting a new form

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -26,7 +26,16 @@ class RegisterForm extends Form {
   doSubmit = async () => {
     //call the server
     //console.log("submitted");
-    await userService.register(this.state.data);
+    try{
+      await userService.register(this.state.data);
+
+    }
+    catch (ex){
+      if(ex.response && ex.response.status==400)
+        const errors ={...this.state.errors};
+        errors.username=ex.response.data;
+        this.setState({errors});
+    }
   };
 
   render() {
