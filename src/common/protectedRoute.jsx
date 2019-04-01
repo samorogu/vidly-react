98c315e92e43949cff7ee48526bb9bbcee567c65
™samorogu™ <samorogu@gmail.com>
2019-03-31 23:20:31 -0600

    adding the protectedRoute.jsx

---
+++ src/common/protectedRoute.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import auth from "../services/authService";
+import { Route, Redirect } from "react-router-dom";
+
+const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
+  return (
+    <Route
+      //path={path}//with the reest operator we will take the path
+      {...rest}
+      render={props => {
+        if (!auth.getCurrentUser()) return <Redirect to="/login" />;
+        return Component ? <Component {...props} /> : render(props);
+      }}
+    />
+  );
+};
+
+export default ProtectedRoute;
